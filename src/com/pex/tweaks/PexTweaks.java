1db087d7c2b59b75505c702cfb28fde16534bfd9
Emanuele Scarlata <scarlataemanuele@gmail.com>
2022-01-30 17:42:12 +0100

    Pex-tweaks: rebrand 2.0
    I have forgot to rebrand the src directory in previuos commit
    
    Signed-off-by: Emanuele Scarlata <scarlataemanuele@gmail.com>

---
+++ src/com/pex/tweaks/PexTweaks.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2016 The Pure Nexus Project
+ * used for Nitrogen OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pex.tweaks;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.Surface;
+import android.preference.Preference;
+import com.android.settings.R;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class PexTweaks extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.pex_tweaks);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.PEX;
+    }
+}
